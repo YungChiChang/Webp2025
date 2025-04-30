b62231ad74ba2da03ca8f87ad66f809d2bf13bd9
B1229001 <144075632+YungChiChang@users.noreply.github.com>
2025-04-30 15:47:38 +0800

    Add files via upload

---
+++ lab12/src/App.js
@@ -0,0 +1,23 @@
+//import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import logo from './logo.svg';
+import './App.css';
+import MultiButton from './multiButton'
+import HelloCGU from './cgu_hello';
+
+function App() {
+  return (
+    <div className="APP">
+      <div>
+        <HelloCGU />
+      </div>
+      <div>
+        <MultiButton num={10} />
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
